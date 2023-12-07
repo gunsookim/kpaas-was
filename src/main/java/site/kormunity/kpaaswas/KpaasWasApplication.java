cb7fc93f19214ca44278b79efa93d857e29d3ab3
gunsookim <ccpc4896@naver.com>
2023-12-07 20:22:54 +0900

    Initial commit

---
+++ src/main/java/site/kormunity/kpaaswas/KpaasWasApplication.java
@@ -0,0 +1,13 @@
+package site.kormunity.kpaaswas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpaasWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpaasWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/kormunity/kpaaswas/KpaasWasApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kormunity.kpaaswas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpaasWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
