1646ab8fde29e59d27a39d5915bfd86949a4ebd4
gunsookim <ccpc4896@naver.com>
2023-12-08 00:00:55 +0900

    .

--- src/main/java/site/kormunity/kpaaswas/KpaasWasApplication.java
+++ src/main/java/site/kormunity/kpaaswas/KpaasWasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KpaasWasApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KpaasWasApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KpaasWasApplication.class, args);
+	}
 
 }
